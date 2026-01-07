589608368564b7a85d392265c7ef5ee45bfd9f16
George.Ashmore <georgeashmore1@gmail.com>
2026-01-07 23:44:59 +0000

    Removed comments

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -6,32 +6,6 @@ return {
   config = function()
     -- Treesitter sytax highlighting tool
     local ts = require("nvim-treesitter")
-    --ts.setup({
-    --  ensure_installed = {
-    --    "bash",
-    --    "c",
-    --    "cpp",
-    --    "css",
-    --    "dockerfile",
-    --    "go",
-    --    "helm",
-    --    "html",
-    --    "java",
-    --    "javascript",
-    --    "json",
-    --    "lua",
-    --    "python",
-    --    "regex",
-    --    "terraform",
-    --    "tmux",
-    --    "vim",
-    --    "vimdoc",
-    --    "yaml",
-    --  },
-    --  sync_install = true,
-    --  highlight = { enable = true },
-    --  indent = { enable = true },
-    --})
     ts.setup({"~/.local/share/nvim/tree-sitter"})
     ts.install(
       {
